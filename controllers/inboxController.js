d37b79e540f87dbdce8038e122088f63b66359ce
suportdevs <suportdevs@gmail.com>
2022-09-18 09:22:46 +0600

    working on user search

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -2,6 +2,13 @@ function getInbox(req, res, next) {
   res.render("inbox.ejs");
 }
 
+async function sarchUsers(req, res, next) {
+  const user = req.body.user;
+  const searchquery = user.replace("+88", "");
+
+  const name_search_regex = new RegExp();
+}
+
 module.exports = {
   getInbox,
 };

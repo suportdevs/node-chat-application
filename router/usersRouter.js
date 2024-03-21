5abba1921e38fd805aa311789d174ec35f213564
suportdevs <suportdevs@gmail.com>
2024-03-22 02:33:44 +0600

    added @multer to upload user avatar

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,7 +4,10 @@ const router = require("express").Router();
 // internal imports
 const { getUsers } = require("../controllers/UsersController");
 const decorateHtmlResponse = require("../middlewares/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 // get users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+router.post("/", avatarUpload);
 
 module.exports = router;

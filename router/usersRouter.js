ed8c6c497799cd9321d97edee5de9ac3c6d5a973
suportdevs <suportdevs@gmail.com>
2024-03-22 17:08:15 +0600

    people add validators and add people validation handler

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -5,9 +5,13 @@ const router = require("express").Router();
 const { getUsers } = require("../controllers/UsersController");
 const decorateHtmlResponse = require("../middlewares/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 // get users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
-router.post("/", avatarUpload);
+router.post("/", avatarUpload, addUserValidators, addUserValidationHandler);
 
 module.exports = router;

205c02cce9704acc2891f0a1233b19cd43126487
suportdevs <suportdevs@gmail.com>
2024-03-27 22:38:43 +0600

    search user to create conversation

--- controllers/InboxController.js
+++ controllers/InboxController.js
@@ -1,6 +1,54 @@
+// internal imports
+const createError = require("http-errors");
+
+// external imports
+const User = require("../models/People");
+
 // get inbox page
 function getInbox(req, res, next) {
   res.render("inbox");
 }
 
-module.exports = { getInbox };
+async function searchUsers(req, res, next) {
+  const user = req.body.user;
+  const searchQuery = user.replace("+88", "");
+
+  const name_search_regex = new RegExp(escape(searchQuery), "i");
+  const mobile_search_regex = new RegExp("^" + escape(searchQuery));
+  const email_search_regex = new RegExp("^" + escape(searchQuery) + "$", "i");
+
+  console.log(mobile_search_regex);
+  try {
+    if (searchQuery !== "") {
+      const users = await User.find(
+        {
+          $or: [
+            {
+              name: name_search_regex,
+            },
+            {
+              mobile: "+88" + mobile_search_regex,
+            },
+            {
+              email: email_search_regex,
+            },
+          ],
+        },
+        "name avatar"
+      );
+      res.json(users);
+    } else {
+      throw createError("You should provide any text to search");
+    }
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+}
+
+module.exports = { getInbox, searchUsers };
--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -2,11 +2,13 @@
 const router = require("express").Router();
 
 // internal imports
-const { getInbox } = require("../controllers/InboxController");
+const { getInbox, searchUsers } = require("../controllers/InboxController");
 const authenticated = require("../middlewares/authenticated");
 const decorateHtmlResponse = require("../middlewares/decorateHtmlResponse");
 
 // get inbox page
 router.get("/", decorateHtmlResponse("Inbox"), authenticated, getInbox);
+
+router.post("/search", authenticated, searchUsers);
 
 module.exports = router;

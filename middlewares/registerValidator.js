cf49a89de36020fb5b12e8cda1b3cf03486185b5
suportdevs <suportdevs@gmail.com>
2024-04-22 22:43:24 +0600

    added register validator and register validation handler

---
+++ middlewares/registerValidator.js
@@ -0,0 +1,59 @@
+// internal imports
+const { check, validationResult } = require("express-validator");
+const createError = require("http-errors");
+const path = require("path");
+const { unlink } = require("fs");
+
+// external imports
+const User = require("../models/People");
+
+const registerValidators = [
+  check("name")
+    .isLength({ min: 3 })
+    .withMessage("Name is required!")
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage("Name must not contain anything other then Alphabet.")
+    .trim(),
+  check("email")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          throw createError("Email already is ues!");
+        }
+      } catch (error) {
+        throw createError(error.message);
+      }
+    }),
+  check("mobile")
+    .isMobilePhone("bn-BD")
+    .withMessage("Mobile must be a valid Bangladeshi mobile number"),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 charactors long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol."
+    ),
+];
+
+function registerValidationHandler(req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    // remove uploaded avatar file
+    if (req.files.length > 0) {
+      const filename = req.files[0];
+      unlink(
+        path.join(__dirname, `../public/uploads/avatars/${filename}`),
+        (err) => console.log(err)
+      );
+    }
+  }
+  res.render("register", { errors: mappedErrors });
+}
+
+module.exports = { registerValidators, registerValidationHandler };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,11 +8,21 @@ const {
   doRegister,
 } = require("../controllers/authController");
 const decoratedHtmlResponse = require("../middlewares/decoratedHtmlResponse");
+const {
+  registerValidators,
+  registerValidationHandler,
+} = require("../middlewares/registerValidator");
 
 router.get("/", decoratedHtmlResponse("Login"), getLogin);
 
 router.get("/register", decoratedHtmlResponse("Register"), getRegister);
 
-router.post("/register", decoratedHtmlResponse("Register"), doRegister);
+router.post(
+  "/register",
+  decoratedHtmlResponse("Register"),
+  registerValidators,
+  registerValidationHandler,
+  doRegister
+);
 
 module.exports = router;

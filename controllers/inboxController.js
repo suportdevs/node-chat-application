e04ba6fb5e05165d4fff0ebfafec1d72104e4171
suportdevs <suportdevs@gmail.com>
2024-04-25 08:43:09 +0600

    conversation user search

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -1,16 +1,38 @@
+const User = require("../models/People");
+const escape = require("../utilies/escape");
+
 function getInbox(req, res, next) {
   try {
-    res.render("inbox", { title: "Inbox - Chat Application" });
+    res.render("inbox");
   } catch (error) {
     next(error);
   }
 }
 
 async function searchUsers(req, res, next) {
+  const { user } = req.body;
+  const searchQuery = user.replace("+88", "");
   try {
-    const { user } = req.body;
-    if (!user) throw createError("Please type anything.");
-  } catch (error) {}
+    if (!searchQuery)
+      throw createError("You should provide any text to search.");
+    const name_search_regex = new RegExp(escape(searchQuery), "i");
+    const mobile_search_regex = new RegExp("^" + escape("+88" + searchQuery));
+    const email_search_regex = new RegExp("^" + escape(searchQuery) + "$", "i");
+
+    const users = await User.find(
+      {
+        $or: [
+          { name: name_search_regex },
+          { mobile: mobile_search_regex },
+          { email: email_search_regex },
+        ],
+      },
+      "name avatar"
+    );
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ errors: { common: { msg: error.message } } });
+  }
 }
 
-module.exports = { getInbox };
+module.exports = { getInbox, searchUsers };
--- routes/inboxRouter.js
+++ routes/inboxRouter.js
@@ -2,10 +2,12 @@
 const router = require("express").Router();
 
 // internal imports
-const { getInbox } = require("../controllers/inboxController");
+const { getInbox, searchUsers } = require("../controllers/inboxController");
 const authenticated = require("../middlewares/authenticated");
 const decoratedHtmlResponse = require("../middlewares/decoratedHtmlResponse");
 
 router.get("/", decoratedHtmlResponse("Inbox"), authenticated, getInbox);
+
+router.post("/search", searchUsers);
 
 module.exports = router;
